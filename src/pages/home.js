4b054c56f2eac02bcd0f46d5879aecc02482909b
Hoon Oh <shoh@help-me.kr>
2021-01-20 19:40:51 +0900

    project init

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,28 +10,8 @@ export default function Home({ children }) {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <Header>
-      <Header.Frame>
-        <Header.Logo to={ROUTES.HOME} alt="YouFun" src={logo} />
-        <Header.Mobile />
-        <Header.NavMenu>
-          <Header.NavItem>
-            <Header.NavScroll to="/">홈</Header.NavScroll>
-          </Header.NavItem>
-          <Header.NavItem>
-            <Header.NavScroll to="/">왜 YouFun?</Header.NavScroll>
-          </Header.NavItem>
-          <Header.NavItem>
-            <Header.NavScroll to="/">YouFun 소개</Header.NavScroll>
-          </Header.NavItem>
-          <Header.NavItem>
-            <Header.NavScroll to="/">사용방법</Header.NavScroll>
-          </Header.NavItem>
-          <Header.NavItem>
-            <Header.CTAButton to="/register">시작하기</Header.CTAButton>
-          </Header.NavItem>
-        </Header.NavMenu>
-      </Header.Frame>
-    </Header>
+    <div class="container">
+      <h1>Hello World</h1>
+    </div>
   );
 }
